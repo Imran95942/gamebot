82b7e205b6860693d11c67ce6e4edf76796ee38f
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-08 17:18:13 +0300

    Update Russianroulette.php

--- src/Entity/Game/Russianroulette.php
+++ src/Entity/Game/Russianroulette.php
@@ -163,7 +163,7 @@ class Russianroulette extends Game
                 $hit = $arg;
 
                 if ($this->saveData($this->data)) {
-                    return $this->editMessage($gameOutput . PHP_EOL . PHP_EOL . __('{PLAYER_HOST} is waiting for opponent to join...', ['{PLAYER_HOST}' => $this->getUserMention('host')]) . PHP_EOL . __('Press {BUTTON} button to join.', ['{BUTTON}' => '<b>\'' . __('Join') . '\'</b>']), $this->customGameKeyboard($hit));
+                    return $this->editMessage($gameOutput . PHP_EOL . PHP_EOL . __('{PLAYER_HOST} ждет, пока противник присоединится...', ['{PLAYER_HOST}' => $this->getUserMention('host')]) . PHP_EOL . __('Press {BUTTON} button to join.', ['{BUTTON}' => '<b>\'' . __('Join') . '\'</b>']), $this->customGameKeyboard($hit));
                 }
             }
 
